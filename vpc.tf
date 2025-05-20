c0229d9557159b36e0f1edf07b538c28c2264a87
mounika <lingammounika2@gmail.com>
2025-05-20 11:58:55 +0530

    chnages to route table

--- vpc.tf
+++ vpc.tf
@@ -27,12 +27,12 @@ resource "aws_internet_gateway" "my_igw" {
 
 
 resource "aws_route_table" "my_rt" {
-    vpc_id = aws_vpc.my_vpc.id
-    route = {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.my_igw.id    
-    }
-  
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_igw.id
+  }
 }
 
 resource "aws_route_table_association" "my_rt_asc" {
